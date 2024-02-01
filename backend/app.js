fb36fe82b3cb894abcd85d29fc35730cba9e2b2a
Priyanshu <Priyanshukumarofficials@gmail.com>
2024-02-01 15:14:17 +0530

    Updating Files

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: 'https://chat-codebot.netlify.app/',
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
